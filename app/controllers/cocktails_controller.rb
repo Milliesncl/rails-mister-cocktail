3d962b0c7b7f8a117c36aa34419f2dbd2d400cbd
Millie Senecal <millie.senecal@gmail.com>
2020-08-16 19:14:30 -0400

    add style scss

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,17 +9,15 @@ class CocktailsController < ApplicationController
     @dose = Dose.new
   end
 
-  def new
-  end
-
   def create
     @cocktail = Cocktail.new(params_cocktail)
-    @cocktail.save
-
-    redirect_to cocktail_path(@cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      redirect_to cocktails_path
+    end
   end
 
-  private
 
   def params_cocktail
     params.require(:cocktail).permit(:name, :photo)
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,15 +1,17 @@
 class DosesController < ApplicationController
   def new
-    @dose = Dose.new
+    @doses = Dose.new
   end
 
   def create
     @dose = Dose.new(params_doses)
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
-    @dose.save
-
-    redirect_to cocktail_path(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      render 'cocktails/show'
+    end
   end
 
   def destroy
